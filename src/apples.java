1ec09003a5de38f5f13e0727edbd82593edf02bd
Chady Aboulhosn <aboulhosnc@vcu.edu>
2018-04-05 21:41:20 -0400

    Scanner video6

--- src/apples.java
+++ src/apples.java
@@ -1,12 +1,11 @@
-
+import java.util.Scanner;
 public class apples {
-	public static void main(String args[]) {
-		double tuna;
-		tuna =  5.28;
-		System.out.print("I want ");
-		System.out.print(tuna);
-		System.out.println(" movies");
-		System.out.println("apples");
-	}
+	
+public static void main(String args[]) {
+	Scanner chady = new Scanner(System.in);
+	
+	System.out.println(chady.nextLine());
+	
+}
 
 }
