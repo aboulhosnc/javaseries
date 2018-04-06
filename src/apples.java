28c8f0986fded5de338865d19a1313bdc22acc17
Chady Aboulhosn <aboulhosnc@vcu.edu>
2018-04-05 22:24:37 -0400

    calculator video7

--- src/apples.java
+++ src/apples.java
@@ -3,9 +3,16 @@ public class apples {
 	
 public static void main(String args[]) {
 	Scanner chady = new Scanner(System.in);
+	double fnum, snum, answer;
 	
-	System.out.println(chady.nextLine());
+	System.out.println("Enter first number: ");
+	fnum = chady.nextDouble();
 	
+	System.out.println("Enter second number: ");
+	snum = chady.nextDouble();
+	
+	answer = fnum + snum;
+	System.out.println(answer);
 }
 
 }
