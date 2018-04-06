35f12d655e68092a3ce0b51fd80a35eafda21ca7
Chady Aboulhosn <aboulhosnc@vcu.edu>
2018-04-05 21:30:34 -0400

    Finished with Video 5

--- src/apples.java
+++ src/apples.java
@@ -1,4 +1,12 @@
 
 public class apples {
+	public static void main(String args[]) {
+		double tuna;
+		tuna =  5.28;
+		System.out.print("I want ");
+		System.out.print(tuna);
+		System.out.println(" movies");
+		System.out.println("apples");
+	}
 
 }
